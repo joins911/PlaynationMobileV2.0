e7ef17d230adce43e214b5f7458610cf800b7149
Joins911 <georgit2007@gmail.com>
2013-09-23 09:50:38 +0200

    last found error

--- src/com/myapps/playnation/Adapters/NotificationAdapter.java
+++ src/com/myapps/playnation/Adapters/NotificationAdapter.java
@@ -15,6 +15,8 @@ import android.widget.TextView;
 
 import com.myapps.playnation.R;
 import com.myapps.playnation.Classes.Keys;
+import com.myapps.playnation.Operations.Configurations;
+import com.myapps.playnation.Operations.DataConnector;
 import com.myapps.playnation.Operations.HelperClass;
 
 public class NotificationAdapter extends BaseAdapter implements MyBaseAdapter {
@@ -22,12 +24,17 @@ public class NotificationAdapter extends BaseAdapter implements MyBaseAdapter {
 	ArrayList<Bundle> notifDataCollection;
 	int count;
 	boolean showMore = true;
+	private boolean showButtonCondition = false;
+	private boolean friendCondition = false;
+	private boolean groupCondition = false;
+	private DataConnector con;
 
 	public NotificationAdapter(Activity act, ArrayList<Bundle> map) {
 		this.notifDataCollection = map;
 		inflater = (LayoutInflater) act
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		count = 10;
+		con = DataConnector.getInst();
 	}
 
 	@Override
@@ -59,26 +66,54 @@ public class NotificationAdapter extends BaseAdapter implements MyBaseAdapter {
 		Button btnAccept = (Button) mView.findViewById(R.id.btnNotAccept);
 		Button btnDecline = (Button) mView.findViewById(R.id.btnNotDecline);
 		if (notifDataCollection != null) {
-			Bundle arg = notifDataCollection.get(position);
+			final Bundle arg = notifDataCollection.get(position);
+			checkNotifiState(arg, arg.getString(Keys.NotificationType));
+			if (showButtonCondition) {
+				btnAccept.setVisibility(View.VISIBLE);
+				btnAccept.setVisibility(View.VISIBLE);
+			} else {
+				btnAccept.setVisibility(View.GONE);
+				btnAccept.setVisibility(View.GONE);
+			}
 			txMsg.setText(arg.getString(Keys.NotificationType));
 			txMsgCon.setText(HelperClass.returnNotificationMessage(arg));
 			txDate.setText(arg.getString(Keys.NotificationTime));
 
+			btnAccept.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					checkNotifiState(arg, arg.getString(Keys.NotificationType));
+					if (friendCondition)
+						con.functionQuery(Configurations.CurrentPlayerID,
+								arg.getString(Keys.NotificationID_FROM),
+								"friendFunctions.php",
+								Keys.POSTFUNCOMMANTACCEPT, "");
+					else if (groupCondition)
+						con.functionQuery(Configurations.CurrentPlayerID,
+								arg.getString(Keys.NotificationID_FROM),
+								"groupFunction.php", Keys.POSTFUNCOMMANTACCEPT,
+								"");
+				}
+			});
+			btnDecline.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					checkNotifiState(arg, arg.getString(Keys.NotificationType));
+					if (friendCondition)
+						con.functionQuery(Configurations.CurrentPlayerID,
+								arg.getString(Keys.NotificationID_FROM),
+								"friendFunctions.php",
+								Keys.POSTFUNCOMMANTDecline, "");
+					else if (groupCondition)
+						con.functionQuery(Configurations.CurrentPlayerID,
+								arg.getString(Keys.NotificationID_FROM),
+								"groupFunction.php",
+								Keys.POSTFUNCOMMANTDecline, "");
+				}
+			});
 		}
-		btnAccept.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				System.out.println("Accept");
-			}
-		});
-		btnDecline.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				System.out.println("Decline");
-			}
-		});
 		return mView;
 	}
 
@@ -101,6 +136,19 @@ public class NotificationAdapter extends BaseAdapter implements MyBaseAdapter {
 	@Override
 	public ArrayList<Bundle> getList() {
 		return notifDataCollection;
+	}
+
+	private void checkNotifiState(Bundle arg, String type) {
+		if (arg.getString(Keys.NotificationType).equals("FriendRequest")) {
+			showButtonCondition = true;
+			friendCondition = true;
+		}
+		if (arg.getString(Keys.NotificationType).equals("GroupApplication")) {
+			showButtonCondition = true;
+			groupCondition = true;
+		} else {
+			showButtonCondition = false;
+		}
 	}
 
 }
